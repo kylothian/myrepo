fb193e2f4b57b6e1f04a4c4d903b551517c45561
Johan van der Westhuizen <jvdwhunter@gmail.com>
2017-11-28 15:16:35 +0200

    updating scripts

---
+++ scripts/denyrootlogon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i '/PermitRootLogin prohibit-password/s/^#//g' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin yes/s/^/#/g' /etc/ssh/sshd_config
+
+service ssh restart
+echo "Login is now Denied"
